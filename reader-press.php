bf198bfdb8fb91eb198632494bf98b939b62bce0
Andrew Jesaitis <andrew@andrewjesaitis.com>
2021-04-30 11:35:22 -0700

    Remove email

--- reader-press.php
+++ reader-press.php
@@ -8,7 +8,7 @@ Author: Andrew Jesaitis
 Author URI: http://andrewjesaitis.com
 */
 
-//Copyright 2010  Andrew Jesaitis  (email : andrew@andrewjesaitis.com)
+//Copyright 2010  Andrew Jesaitis
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
